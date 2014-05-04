da0a3de16e212408ec2b4f76869ba6903e8b1cf2
Vangos <pterneas@gmail.com>
2014-05-05 00:51:28 +0300

    Initial commit

---
+++ KinectCoordinateMapping/KinectCoordinateMapping/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectCoordinateMapping
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        IList<Body> _bodies;
+
+        CameraMode _mode = CameraMode.Color;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.Default;
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+            }
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_bodies != null)
+            {
+                if (_bodies.Count() > 0)
+                {
+                    foreach (var body in _bodies)
+                    {
+                        body.Dispose();
+                    }
+                }
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            // Color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == CameraMode.Color)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Depth
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == CameraMode.Depth)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Infrared
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == CameraMode.Infrared)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Body
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    canvas.Children.Clear();
+
+                    _bodies = new Body[frame.BodyFrameSource.BodyCount];
+
+                    frame.GetAndRefreshBodyData(_bodies);
+
+                    foreach (var body in _bodies)
+                    {
+                        if (body.IsTracked)
+                        {
+                            // COORDINATE MAPPING
+                            foreach (Joint joint in body.Joints.Values)
+                            {
+                                if (joint.TrackingState == TrackingState.Tracked)
+                                {
+                                    // 3D space point
+                                    CameraSpacePoint jointPosition = joint.Position;
+
+                                    // 2D space point
+                                    Point point = new Point();
+
+                                    if (_mode == CameraMode.Color)
+                                    {
+                                        ColorSpacePoint colorPoint = _sensor.CoordinateMapper.MapCameraPointToColorSpace(jointPosition);
+
+                                        point.X = float.IsInfinity(colorPoint.X) ? 0 : colorPoint.X;
+                                        point.Y = float.IsInfinity(colorPoint.Y) ? 0 : colorPoint.Y;
+                                    }
+                                    else if (_mode == CameraMode.Depth || _mode == CameraMode.Infrared)
+                                    {
+                                        DepthSpacePoint depthPoint = _sensor.CoordinateMapper.MapCameraPointToDepthSpace(jointPosition);
+
+                                        point.X = float.IsInfinity(depthPoint.X) ? 0 : depthPoint.X;
+                                        point.Y = float.IsInfinity(depthPoint.Y) ? 0 : depthPoint.Y;
+                                    }
+
+                                    // Draw
+                                    Ellipse ellipse = new Ellipse
+                                    {
+                                        Fill = Brushes.LightBlue,
+                                        Width = 20,
+                                        Height = 20
+                                    };
+
+                                    Canvas.SetLeft(ellipse, point.X - ellipse.Width / 2);
+                                    Canvas.SetTop(ellipse, point.Y - ellipse.Height / 2);
+
+                                    canvas.Children.Add(ellipse);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    enum CameraMode
+    {
+        Color,
+        Depth,
+        Infrared
+    }
+}
---
+++ KinectCoordinateMapping/KinectCoordinateMapping/Utilities/ColorExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Media;
+using Microsoft.Kinect;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace KinectCoordinateMapping
+{
+    /// <summary>
+    /// Provides some common functionality for manipulating color frames.
+    /// </summary>
+    public static class ColorExtensions
+    {
+        #region Members
+
+        /// <summary>
+        /// The bitmap source.
+        /// </summary>
+        static WriteableBitmap _bitmap = null;
+
+        /// <summary>
+        /// Frame width.
+        /// </summary>
+        static int _width;
+
+        /// <summary>
+        /// Frame height.
+        /// </summary>
+        static int _height;
+
+        /// <summary>
+        /// The RGB pixel values.
+        /// </summary>
+        static byte[] _pixels = null;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Converts a color frame to a System.Media.Imaging.BitmapSource.
+        /// </summary>
+        /// <param name="frame">The specified color frame.</param>
+        /// <returns>The specified frame in a System.Media.Imaging.BitmapSource representation of the color frame.</returns>
+        public static BitmapSource ToBitmap(this ColorFrame frame)
+        {
+            if (_bitmap == null)
+            {
+                _width = frame.FrameDescription.Width;
+                _height = frame.FrameDescription.Height;
+                _pixels = new byte[_width * _height * Constants.BYTES_PER_PIXEL];
+                _bitmap = new WriteableBitmap(_width, _height, Constants.DPI, Constants.DPI, Constants.FORMAT, null);
+            }
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(_pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(_pixels, ColorImageFormat.Bgra);
+            }
+
+            _bitmap.Lock();
+
+            Marshal.Copy(_pixels, 0, _bitmap.BackBuffer, _pixels.Length);
+            _bitmap.AddDirtyRect(new Int32Rect(0, 0, _width, _height));
+
+            _bitmap.Unlock();
+
+            return _bitmap;
+        }
+
+        #endregion
+    }
+}
